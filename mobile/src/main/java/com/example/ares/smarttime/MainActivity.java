bb8dddccbf794c01d89e9c2cae79bff92e73df3a
JCLengeling <Joel.Cedric@lengeling.eu>
2015-03-29 21:10:28 +0200

    Bug fixes and swap between time modes

--- mobile/src/main/java/com/example/ares/smarttime/MainActivity.java
+++ mobile/src/main/java/com/example/ares/smarttime/MainActivity.java
@@ -8,11 +8,15 @@ import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private boolean decimalMode, binaryMode, octalMode, hexMode;
 
     private char octal[]={'0','1','2','3','4','5','6','7'};
     private char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
@@ -22,6 +26,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        decimalMode = true;
+        binaryMode = octalMode = hexMode = false;
+        setContentView(R.layout.activity_decimal);
 
         Thread t = new Thread(){
             public void run(){
@@ -31,20 +38,50 @@ public class MainActivity extends ActionBarActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                current = new GregorianCalendar();
-                                setContentView(R.layout.activity_main);
-                                TextView number = (TextView)findViewById(R.id.clockNumberText);
-                                TextView bin = (TextView)findViewById(R.id.clockBinText);
-                                TextView octa = (TextView)findViewById(R.id.clockOctaText);
-                                TextView hex = (TextView)findViewById(R.id.clockHexText);
+                                TimeZone tz = TimeZone.getDefault();
 
-                                int minutes =  (int)((current.getTimeInMillis()/(1000*60))% 60);
+                                current = new GregorianCalendar(tz);
 
-                                number.setText(String.valueOf(minutes));
-                                bin.setText(transformToBinary(minutes));
-                                octa.setText(transformToOctal(minutes));
-                                hex.setText(transformToHex(minutes));
 
+                                int hours   =   current.get(Calendar.HOUR_OF_DAY);
+                                int minutes =   current.get(Calendar.MINUTE);
+                                int second  =   current.get(Calendar.SECOND);
+
+
+                                if(decimalMode){
+                                    TextView hourTextView = (TextView)findViewById(R.id.clockHour);
+                                    TextView minuteTextView = (TextView)findViewById(R.id.clockMinute);
+                                    TextView secondTextView = (TextView)findViewById(R.id.clockSecond);
+
+
+                                    hourTextView.setText(String.valueOf(hours));
+                                    minuteTextView.setText(String.valueOf(minutes));
+                                    secondTextView.setText(String.valueOf(second));
+                                }else if (binaryMode){
+                                    TextView hourTextView = (TextView)findViewById(R.id.clockHour);
+                                    TextView minuteTextView = (TextView)findViewById(R.id.clockMinute);
+                                    TextView secondTextView = (TextView)findViewById(R.id.clockSecond);
+
+                                    hourTextView.setText(transformToBinary(hours));
+                                    minuteTextView.setText(transformToBinary(minutes));
+                                    secondTextView.setText(transformToBinary(second));
+                                }else if (octalMode){
+                                    TextView hourTextView = (TextView)findViewById(R.id.clockHour);
+                                    TextView minuteTextView = (TextView)findViewById(R.id.clockMinute);
+                                    TextView secondTextView = (TextView)findViewById(R.id.clockSecond);
+
+                                    hourTextView.setText(transformToOctal(hours));
+                                    minuteTextView.setText(transformToOctal(minutes));
+                                    secondTextView.setText(transformToOctal(second));
+                                }else if (hexMode){
+                                    TextView hourTextView = (TextView)findViewById(R.id.clockHour);
+                                    TextView minuteTextView = (TextView)findViewById(R.id.clockMinute);
+                                    TextView secondTextView = (TextView)findViewById(R.id.clockSecond);
+
+                                    hourTextView.setText(transformToHex(hours));
+                                    minuteTextView.setText(transformToHex(minutes));
+                                    secondTextView.setText(transformToHex(second));
+                                }
                             }
                         });
                     }
@@ -71,6 +108,8 @@ public class MainActivity extends ActionBarActivity {
     }
     private String transformToOctal(int value ){
         String ret ="";
+        if (value == 0)
+            ret = "0";
         while(value >0){
             int tmp = value %8;
             ret = octal[tmp]+ret;
@@ -80,6 +119,8 @@ public class MainActivity extends ActionBarActivity {
     }
     private String transformToHex(int value){
         String ret="";
+        if (value == 0)
+            ret = "0";
         while(value >0){
             int tmp = value %16;
             ret = hex[tmp]+ret;
@@ -104,7 +145,25 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_decimal) {
+            setContentView(R.layout.activity_decimal);
+            decimalMode = true;
+            binaryMode = octalMode = hexMode = false;
+            return true;
+        }else if (id == R.id.action_binary) {
+            setContentView(R.layout.activity_binary);
+            binaryMode = true;
+            decimalMode = octalMode = hexMode = false;
+            return true;
+        }else if (id == R.id.action_octal) {
+            setContentView(R.layout.activity_octal);
+            octalMode = true;
+            decimalMode = binaryMode = hexMode = false;
+            return true;
+        }else if (id == R.id.action_hex) {
+            setContentView(R.layout.activity_hex);
+            hexMode = true;
+            decimalMode = octalMode = binaryMode = false;
             return true;
         }
 
